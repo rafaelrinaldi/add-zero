ef0f071530795a39792e2370486f82909868e974
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-13 11:31:27 -0300

    Added yet another test for 3 decimal places.

--- test.js
+++ test.js
@@ -7,5 +7,6 @@ test('add zero', function(t) {
   t.deepEqual(addZero(5), '05', 'default decimal places should be 2');
   t.deepEqual(addZero(10), '10', 'only add zero if not enough decimal places');
   t.deepEqual(addZero(5, 3), '005', 'test 3 decimal places');
+  t.deepEqual(addZero(100, 3), '100', 'test 3 decimal places max digits');
   t.end();
 });
