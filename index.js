5b2c45ac70c16056da23233468c28a5cdd0005d3
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-13 14:11:10 -0300

    Improved zero repeating strategy.
    No need for conditionals now, just handle the `size` value.

--- index.js
+++ index.js
@@ -7,15 +7,15 @@
 
     var isNegative = Number(value) < 0;
     var buffer = value.toString();
+    var size = 0;
 
     // Strip minus sign if number is negative
     if(isNegative) {
       buffer = buffer.slice(1);
     }
 
-    if(buffer.length < digits) {
-      buffer = new Array(digits).join('0') + buffer;
-    }
+    size = digits - buffer.length + 1;
+    buffer = new Array(size).join('0').concat(buffer);
 
     // Adds back minus sign if needed
     return (isNegative ? '-' : '') + buffer;
--- test.js
+++ test.js
@@ -3,11 +3,21 @@
 var addZero = require('./');
 var test = require('tape');
 
+// console.log(addZero(5), '05');
+// console.log(addZero(10), '10');
+// console.log(addZero(5, 3), '005');
+// console.log(addZero(100, 3), '100');
+// console.log(addZero(0), '00');
+// console.log(addZero(-5), '-05');
+// console.log(addZero(-10), '-10');
+// console.log(addZero(-5, 3), '-005');
+
 test('add zero', function(t) {
   t.deepEqual(addZero(5), '05', 'default decimal places should be 2');
   t.deepEqual(addZero(10), '10', 'only add zero if not enough decimal places');
   t.deepEqual(addZero(5, 3), '005', 'test 3 decimal places');
   t.deepEqual(addZero(100, 3), '100', 'test 3 decimal places max digits');
+  t.deepEqual(addZero(20, 3), '020', 'test 2 digits number within 3 digit decimal places');
   t.deepEqual(addZero(0), '00', 'test zero value');
   t.deepEqual(addZero(-5), '-05', 'test negative value with default decimal places');
   t.deepEqual(addZero(-10), '-10', 'test negative value when not enough decimal places');
