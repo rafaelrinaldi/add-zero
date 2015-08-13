983db156167b5261241eba4e07218b66add35513
Rafael Rinaldi <rafael@rinaldi.io>
2015-08-13 11:30:39 -0300

    Added test for 3 decimal places.

--- test.js
+++ test.js
@@ -6,5 +6,6 @@ var test = require('tape');
 test('add zero', function(t) {
   t.deepEqual(addZero(5), '05', 'default decimal places should be 2');
   t.deepEqual(addZero(10), '10', 'only add zero if not enough decimal places');
+  t.deepEqual(addZero(5, 3), '005', 'test 3 decimal places');
   t.end();
 });
